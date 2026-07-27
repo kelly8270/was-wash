f1c255fd277a084c549741932f98b617606cbf3f
kiptoo2002 <victor55kiptoo@gmail.com>
2026-07-27 13:00:27 +0300

    commited

--- backend/app.py
+++ backend/app.py
@@ -1239,4 +1239,5 @@ def serve_file(filename):
 if __name__ == '__main__':
     init_db()
     print("Database initialized!")
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
